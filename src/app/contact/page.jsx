5b93da8fd0d0390f11f9ca3519d026700450c0c0
NotZarifGithub <flacko.programming@gmail.com>
2023-10-03 14:46:53 +0800

    contact page done

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import { Dela_Gothic_One, Orbitron } from "next/font/google"
+import Link from "next/link"
+
+// FONT FOR MAIN CONTENT
+const DGO = Dela_Gothic_One({
+  weight: ['400'],
+  subsets: ['latin']
+})
+
+// FONT FOR "CONTACT ME" 
+const orbitron = Orbitron({
+  weight: ['400', '500', '600', '700', '800', '900'],
+  subsets: ['latin']
+})
 
 const contactPage = () => {
   return (
-    <div className="h-[calc(100vh-20px)] md:h-[calc(100vh-30px)] lg:h-[calc(100vh-40px)]">contactPage</div>
+    <section className={`h-[calc(100vh-60px)] md:h-[calc(100vh-90px)] lg:h-[calc(100vh-110px)] max-w-[1100px] m-auto px-[30px]`}>
+      <div className="flex gap-9 flex-col items-center justify-center h-[calc(100vh-60px)] md:h-[calc(100vh-90px)] lg:h-[calc(100vh-110px)]">
+
+        {/* MAIN TITLE */}
+
+        <div className=''>
+          <h1 className={`${DGO.className} text-7xl md:text-[8rem] lg:text-[10rem]`}>
+            Get in <span className="pl-[75px] md:pl-[135px]"> touch</span>
+          </h1>
+        </div>
+
+        {/* INFO */}
+        <div>
+          <div className="">
+            <div className="text-xs font-medium md:text-base">
+              <h1 className={`${orbitron.className} text-sm md:text-xl font-semibold text-black/60 pb-[10px] md:pb-[20px] text-center`}>
+                Contact details
+              </h1>
+              <Link
+                href="mailto:flacko.programming@gmail.com"
+              >
+                <p className="border-black hover:border-b">
+                  flacko.programming@gmail.com
+                </p>
+              </Link>
+              <Link
+                href="mailto:zarif.onlywork@gmail.com"  
+              >
+                <p className="border-black hover:border-b">
+                zarif.onlywork@gmail.com
+                </p>
+              </Link>
+              <p className="cursor-default">
+                +60 12 345 6789
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
